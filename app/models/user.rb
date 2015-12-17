9ba267e1d4729544f5c8b6b2e89ee22d477789b0
Carl Venezia <veneziacarl@gmail.com>
2015-12-17 15:22:30 -0500

    try 3 facebook auth

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,12 +2,29 @@ class User < ActiveRecord::Base
   has_many :questions
   has_many :answers
 
+
   def self.from_omniauth(auth)
-    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
-      user.provider = auth.provider
-      user.uid      = auth.uid
-      user.name     = auth.info.name
-      user.save
-    end
+    provider = auth.provider
+    uid = auth.uid
+
+      find_or_create_by(provider: provider, uid: uid) do |user|
+        user.provider = provider
+        user.uid = uid
+        # user.email = auth.info.email
+        user.name = auth.info.name
+        # user.avatar_url = auth.info.image
+      end
   end
 end
+
+
+
+
+
+
+# where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+#   user.provider = auth.provider
+#   user.uid      = auth.uid
+#   user.name     = auth.info.name
+#   user.save
+# end
